83cb8285dc114062d7a47555e3df44df7c7bde7d
Nikhitha <nikhibeeraka2000@gmail.com>
2024-08-07 21:31:27 +0530

    started with right part of notes

---
+++ pocketnotes/src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from '../components/Home.module.css';
+import notebookImage from '../assets/Home.png';
+import lock from '../assets/lock.png';
+
+function Home() {
+  return (
+    <div className={styles.pocketnotes}>
+        <div className= {styles.right}>
+            <div className={styles.innercontent}>
+              <img src={notebookImage} alt="Notebook" className={styles.notebookImage} />
+              <h1 className={styles.heading}>Pocket Notes</h1>
+              <p className={styles.info}>Send and receive messages without keeping your phone online.</p>
+              <p className={styles.info}>Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
+            </div>
+            <div className={styles.footer}>
+              <img src={lock} alt="lock" className={styles.encryption}/>
+              <span>end-to-end encrypted</span>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
